c4a481d671260088757534782e7fc8d13d564923
Arthur-Rafael-Antunes <130070188+Arthur-Rafael-Antunes@users.noreply.github.com>
2023-11-02 12:04:11 -0300

    Update app.py
    adicionado mensagem de erro caso balanceamento dê errado

--- app.py
+++ app.py
@@ -158,23 +158,26 @@ def balanciamento(reactants, products):
         compoundDecipher(reactants[i],i,1)
     for i in range(len(products)):
         compoundDecipher(products[i],i+len(reactants),-1)
-    elementMatrix = Matrix(elementMatrix)
-    elementMatrix = elementMatrix.transpose()
-    solution=elementMatrix.nullspace()[0]
-    multiple = lcm([val.q for val in solution])
-    solution = multiple*solution
-    coEffi=solution.tolist()
-    output=""
-    for i in range(len(reactants)):
-        output+=str(coEffi[i][0])+reactants[i]
-        if i<len(reactants)-1:
-            output+=" + "
-    output+=" -> "
-    for i in range(len(products)):
-        output+=str(coEffi[i+len(reactants)][0])+products[i]
-        if i<len(products)-1:
-            output+=" + "
-    return output
+    try: 
+        elementMatrix = Matrix(elementMatrix)
+        elementMatrix = elementMatrix.transpose()
+        solution=elementMatrix.nullspace()[0]
+        multiple = lcm([val.q for val in solution])
+        solution = multiple*solution
+        coEffi=solution.tolist()
+        output=""
+        for i in range(len(reactants)):
+            output+=str(coEffi[i][0])+reactants[i]
+            if i<len(reactants)-1:
+                output+=" + "
+        output+=" -> "
+        for i in range(len(products)):
+            output+=str(coEffi[i+len(reactants)][0])+products[i]
+            if i<len(products)-1:
+                output+=" + "
+        return output
+     except Exception as e:
+        return "Erro, o balanciamento não deu certo " 
 
 @app.route('/calculador',  methods=['GET', 'POST'])
 def calculator():
